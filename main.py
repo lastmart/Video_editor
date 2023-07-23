9c5a8abd5e8be0303bec31b7641a1e694dd10350
dimastarsis <kurilin.dmitriy@gmail.com>
2023-07-23 15:30:11 +0500

    add trim, change speed, merge opportunities

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 from moviepy.editor import VideoFileClip, concatenate_videoclips
+from video_editor import *
+from PyQt5.QtWidgets import QApplication, QWidget
+import sys
 
-
-def merge_videos(video_paths, output_path):
-    clips = []
-    for path in video_paths:
-        clip = VideoFileClip(path)
-        clips.append(clip)
-
-    final_clip = concatenate_videoclips(clips)
-    final_clip.write_videofile(output_path)
-
-
-# Пример использования:
 video_paths = [
-    "/Users/dimasta/work/Video_editor/resources/fugu_eat_carrot.mp4",
-    "/Users/dimasta/work/Video_editor/resources/shrek_dancing.mp4"
+   "/Users/dimasta/work/Video_editor/resources/fugu_eat_carrot.mp4",
+   "/Users/dimasta/work/Video_editor/resources/shrek_dancing.mp4"
 ]
+app = QApplication(sys.argv)
+window = QWidget()
+window.show()
+app.exec()
+print(123)
+#merged = merge_clips(clips)
+#save_clip(merged, "1.mp4")
 
-output_path = "merged_video.mp4"
-merge_videos(video_paths, output_path)
+#speeded = set_clip_speed(clips[0], 0.5)
+#save_clip(speeded, "3.mp4")

16ff742a43436d03859f2a75531f19cb87d15870
dimastarsis <kurilin.dmitriy@gmail.com>
2023-07-23 15:30:11 +0500

    add trim, change speed, merge opportunities

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 from moviepy.editor import VideoFileClip, concatenate_videoclips
+from video_editor import *
 
 
-def merge_videos(video_paths, output_path):
-    clips = []
-    for path in video_paths:
-        clip = VideoFileClip(path)
-        clips.append(clip)
-
-    final_clip = concatenate_videoclips(clips)
-    final_clip.write_videofile(output_path)
-
-
-# Пример использования:
 video_paths = [
-    "/Users/dimasta/work/Video_editor/resources/fugu_eat_carrot.mp4",
-    "/Users/dimasta/work/Video_editor/resources/shrek_dancing.mp4"
+   "/Users/dimasta/work/Video_editor/resources/fugu_eat_carrot.mp4",
+   "/Users/dimasta/work/Video_editor/resources/shrek_dancing.mp4"
 ]
 
-output_path = "merged_video.mp4"
-merge_videos(video_paths, output_path)
+clips = open_clips(video_paths)
+#merged = merge_clips(clips)
+#save_clip(merged, "1.mp4")
+trimed = trim_clip(clips[0], (0, 1), (0, 3))
+save_clip(trimed, "2.mp4")
+#speeded = set_clip_speed(clips[0], 0.5)
+#save_clip(speeded, "3.mp4")
---
+++ video_editor.py
@@ -0,0 +1,32 @@
+from moviepy.editor import VideoFileClip, concatenate_videoclips, vfx
+from typing import Union
+
+
+def merge_clips(clips: list) -> VideoFileClip:
+    return concatenate_videoclips(clips)
+
+
+def trim_clip(
+    clip: VideoFileClip,
+    start_time: tuple,
+    end_time: tuple
+) -> VideoFileClip:
+    return clip.subclip(start_time, end_time)
+
+
+def set_clip_speed(clip: VideoFileClip, speed: int) -> VideoFileClip:
+    return clip.fx(vfx.speedx, speed)
+
+
+def save_clip(clip: VideoFileClip, output_path: str) -> None:
+    clip.write_videofile(output_path)
+
+
+def open_clips(input_paths: Union[list, str]) -> list:
+    if isinstance(input_paths, str):
+        return VideoFileClip(input_paths)
+
+    elif isinstance(input_paths, list):
+        return [VideoFileClip(input_path) for input_path in input_paths]
+
+    raise TypeError

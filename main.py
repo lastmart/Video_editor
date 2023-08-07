df9216ad3b70070f7918c563b550dbb85b63d000
BigMommaPr <egoregormart09@gmail.com>
2023-08-07 20:38:55 +0500

    complete merge command

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 from video_editor_parser import Parser
 
 
-video_paths = [
-    r"/mnt/c/Users/egore/OneDrive/Документы/python/python_task/Video_editor/resources/fugu_eat_carrot.mp4",
-    r'/mnt/c/Users/egore/OneDrive/Документы/python/python_task/Video_editor/resources/shrek_dancing.mp4'
-]
-
-
 if __name__ == "__main__":
     parser = Parser()
     args = parser.parse_args()
